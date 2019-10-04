f913ae5ffb265f4470d02ee01988dc1122035743
lois-lee <ll556@cornell.edu>
2019-10-04 12:11:49 -0400

    Update NYQ_init.m

--- NYQ_init.m
+++ NYQ_init.m
@@ -7,10 +7,13 @@ function [sta] = NYQ_init(par,sta)
 % all state variables used by this block must be initialized
 
 % length 16 coeff array:
-sta.NYQ.coeff = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]; %filter coefficents
+% sta.NYQ.coeff = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]; %filter coefficents
 
 % length 24 coeff array:
 % sta.NYQ.coeff = [0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.5,0.6,0.7,0.8,0.9,0.9,0.8,0.7,0.6,0.5,0.4,0.35,0.3,0.25,0.2,0.15,0.1]; %filter coefficents
+
+% length 32 coeff array:
+sta.NYQ.coeff = [0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45, 0.5, 0.55, 0.6,0.65, 0.7,0.75, 0.8,0.9,0.9,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1]; %filter coefficents
 
 sta.NYQ.cnt = 0; % set counter to zero
 sta.NYQ.old_output = 0; % old output
