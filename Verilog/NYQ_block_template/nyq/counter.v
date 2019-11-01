f9b36cc672934ab27ffa96b456f943345ed8b006
TomiKalejaiye <TeeKay3000@gmail.com>
2019-10-31 21:00:54 -0400

    Changed counter to go from 7 and count down.

---
+++ Verilog/NYQ_block_template/nyq/counter.v
@@ -0,0 +1,29 @@
+/* ================================================================================
+// Counter: Counter for NYQ Block
+// Author: Tomi Kalejaiye
+// Date: 10/25/2019
+// Project: SynTech
+// ---- Description ---------------------------------------------------------------
+// A simple counter that goes from 0d to 7d then wraps around.
+//
+// ---- Input/Output specifications -----------------------------------------------
+// Cnt_Out_DO	counter output [3bit]
+// ================================================================================ */
+
+module counter    (
+
+input                       Clk_CI,  // Clock signal
+input                       Rst_RBI, // Asynchronous active low reset
+
+output reg [3:0] Cnt_Out_DO = 3’b111; // Counter value output
+);
+
+//—-Logic—-
+always @(posedge Clk_CI)
+if (Rst_RBI) begin
+  Cnt_Out_DO <= 3’b111;
+end else begin
+  Cnt_Out_DO <= Cnt_Out_DO - 1;
+end
+
+endmodule 
