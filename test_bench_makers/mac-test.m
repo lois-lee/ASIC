78675784e6fd54ec587ba4fc9ee852a07ecd71ea
lois-lee <ll556@cornell.edu>
2019-11-16 23:20:12 -0500

    Update mac-test.m

--- test_bench_makers/mac-test.m
+++ test_bench_makers/mac-test.m
@@ -41,19 +41,24 @@ for tt=1:100
         disp(accumulate_D);
         Out_DO_lst = [Out_DO_lst, sum_D .* (2^bitshift)];
     end
+    
+    % In0_DI = .02;
+    % In1_DI = .03;
     In0_DI = rand(1);
     In1_DI = rand(1);
     
     % Rst_RB, Clr_SI, WrEn_SI,In0_DI,In1_DI
-    formatSpec_input = '1 0 0 %d %d \n';
+    formatSpec_input = '0 0 0 %d %d \n';
     fprintf(fileID,formatSpec_input, In0_DI.* (2^bitshift), ...
         In1_DI.* (2^bitshift));
     
     product_D = RealMULT(In0_DI, In1_DI, FixP_out, QType_out);
     sum_D = RealADD(product_D, accumulate_D, FixP_out, QType_out);
+    accumulate_D = 0;
+    product_D = 0;
     Out_DO_lst = [Out_DO_lst, sum_D .* (2^bitshift)];
     
-    accumulate_D = 0;
+    
     disp(accumulate_D);
 end
 fclose(fileID);
